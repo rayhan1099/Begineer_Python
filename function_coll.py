32d484f5db0a2ae289805689db637683ef0caa8a
rayhan1099 <rayhan.193312@gmail.com>
2022-09-03 23:07:16 +0600

    py file

--- function_coll.py
+++ function_coll.py
@@ -1,5 +1,7 @@
 def func(name):
-    message="Hi "+name;
-    return message;
-name=input("Enter Your Name: ")
+    message="Hi " + name
+    return message
+
+    
+name = input("Enter Your Name: ")
 print(func(name))
--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print("Hello")
+print("thank you")
